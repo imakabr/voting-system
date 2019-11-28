3c5df5b42282ab1787207ab905bdf6255dcf2b2a
imakabr <imakabr@gmail.com>
2019-11-29 00:04:30 +0300

    Add authentication-provider as userService, UserTo

--- src/main/java/ru/imakabr/votingsystem/service/UserService.java
+++ src/main/java/ru/imakabr/votingsystem/service/UserService.java
@@ -3,32 +3,45 @@ package ru.imakabr.votingsystem.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import ru.imakabr.votingsystem.AuthorizedUser;
 import ru.imakabr.votingsystem.model.User;
 import ru.imakabr.votingsystem.repository.UserRepository;
+import ru.imakabr.votingsystem.to.UserTo;
+import ru.imakabr.votingsystem.util.UserUtil;
 
 import java.util.List;
 
+import static ru.imakabr.votingsystem.util.UserUtil.prepareToSave;
 import static ru.imakabr.votingsystem.util.ValidationUtil.checkNotFound;
 import static ru.imakabr.votingsystem.util.ValidationUtil.checkNotFoundWithId;
 
-@Service
-public class UserService {
+@Service("userService")
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserService implements UserDetailsService {
 
     private static final Sort SORT_NAME_EMAIL = new Sort(Sort.Direction.ASC, "name", "email");
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User create(User user) {
         Assert.notNull(user, "user must not be null");
-        return repository.save(user);
+        return repository.save(prepareToSave(user, passwordEncoder));
     }
 
     public void delete(int id) {
@@ -51,7 +64,22 @@ public class UserService {
 
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
-        checkNotFoundWithId(repository.save(user), user.getId());
+        repository.save(prepareToSave(user, passwordEncoder));
+    }
+
+    @Transactional
+    public void update(UserTo userTo) {
+        User user = get(userTo.getId());
+        repository.save(prepareToSave(UserUtil.updateFromTo(user, userTo), passwordEncoder));
+    }
+
+    @Override
+    public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.getByEmail(email.toLowerCase());
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " is not found");
+        }
+        return new AuthorizedUser(user);
     }
 
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -14,8 +14,8 @@ ALTER SEQUENCE VOTE_SEQ RESTART WITH 100000;
 ALTER SEQUENCE ITEM_SEQ RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
-VALUES ('User', 'user@yandex.ru', 'password'),
-       ('Admin', 'admin@gmail.com', 'admin');
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin');
 
 INSERT INTO user_roles (role, user_id)
 VALUES ('ROLE_USER', 100000),
